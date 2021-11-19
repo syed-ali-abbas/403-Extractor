f4845ff9225d7fd84b3b8b5c90c7b94720736978
Syed Ali Abbas <syedaliabbasshahmajeed@gmail.com>
2021-11-19 16:25:53 -0500

    updated

--- 403-URLS-Extractor/403-Extractor.py
+++ 403-URLS-Extractor/403-Extractor.py
@@ -7,35 +7,6 @@ import numpy as np
 from requests.exceptions import HTTPError
 import sys
 
-#pip3 install requests-html
-#https://www.thepythoncode.com/article/extracting-email-addresses-from-web-pages-using-python
-
-
-
-
-#url = input("Enter the URL:- ")
-#EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
-
-
-# initiate an HTTP session
-#session = HTMLSession()
-
-# get the HTTP Response
-#r = session.get(url)
-
-# for JAVA-Script driven websites
-#r.html.render()
-
-
-#for re_match in re.finditer(EMAIL_REGEX, r.html.raw_html.decode()):
-#    print(re_match.group())
-
-#da = pd.read_csv("data.csv")
-#arrayy=np.array(da.iloc[:,1:])
-#for i in arrayy:
-#	print(i)
-
-
 a=sys.argv[1:]
 count=0
 df = pd.read_csv(a[0],error_bad_lines=False)
